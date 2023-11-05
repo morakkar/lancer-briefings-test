57f4936f273611b5d8c57b98a82cf9bd591f99a0
morakkar <149257011+morakkar@users.noreply.github.com>
2023-11-05 01:48:04 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Death-On-The-Glacier",
           "status": "start"
         },
       ],
